3f72e51a473c6da088bd3b3ef13b788f90091cc0
Chris Mangano <chris.mangano@me.com>
2015-05-26 20:20:45 -0400

    Playing around with strong params in exercise controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,8 +1,14 @@
 class ExercisesController < ApplicationController
 	def create
 		@workout = Workout.find(params[:workout_id])
-		@exercise = @workout.exercises.create(params[:exercise].permit(:name, :sets, :reps))
+		@exercise = @workout.exercises.create(exercise_params)
+		#@exercise = @workout.exercises.create(params[:exercise].permit(:name, :sets, :reps))
 
 		redirect_to workout_path(@workout)
+	end
+
+	private
+	def exercise_params
+		params.require(:exercise).permit(:name, :sets, :reps)
 	end
 end
